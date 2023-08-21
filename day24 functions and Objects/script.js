efe4938eb4758e759e6d1ee83be4e4a54b1ca070
Rushimoon <rushikeshmoon1998@gmail.com>
2023-08-21 09:10:56 +0530

    functions

---
+++ day24 functions and Objects/script.js
@@ -0,0 +1,60 @@
+let a=5;
+let b=10;
+console.log(a+b);
+//function examples
+
+
+function wakeup(){
+ console.log("samiksha gets wake up");
+ console.log("gets ready for the day")
+}
+function gotoclass(){
+    console.log("samiksha goes to class");
+}
+function gotoclg(){
+    console.log("samiksha goes to clg");
+}
+function gotohome(){
+    console.log("samiksha goes to home");
+}
+function gotosleep(){
+    console.log("samiksha goes to sleeps");
+}
+
+ console.log("-=-=-=-=-=-=-=monday-=-=--=-=-=-=");
+wakeup();
+gotoclass();
+gotoclg();
+gotohome();
+gotosleep();
+
+console.log("-=-=-=-=-=-=-=tuesday-=-=-=-=-=-=-=-=")
+wakeup();
+gotoclg();
+gotoclass();
+gotohome();
+gotosleep();
+
+
+// calcualtor
+
+
+
+function addOfTwoNum(a,b){
+   console.log("welcome to addition ")
+
+   return a+b ;
+  
+}
+let x=addOfTwoNum(2,3);
+console.log(x)
+console.log(addOfTwoNum(10,20));
+
+
+
+
+
+
+
+
+
