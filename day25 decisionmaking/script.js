da43e382d0ea507aa38401bd4ea46f41daa91a42
Rushimoon <rushikeshmoon1998@gmail.com>
2023-08-22 09:18:50 +0530

    if else if

---
+++ day25 decisionmaking/script.js
@@ -0,0 +1,88 @@
+//Decision maiking 
+
+//if
+//if/else
+//if elseif elseif  else
+//switch
+
+
+//driving licence
+
+
+let age=17;
+ if(age>=18)
+ {
+    console.log("congo you are eligible to drive");
+ }
+ else{
+    console.log("beta thoida bade ho jao aur baad me aao");
+ }
+
+
+
+
+ ///days of week;
+   let day=7;
+  if(day==1){
+   console.log("monday")
+  }
+  else if(day==2){
+  console.log("tuesday")
+  }
+  else if(day==3){
+    console.log("wedneday")
+    }
+  else if(day==4){
+        console.log("thusday")
+        }
+  else if(day==5){
+        console.log("friday")
+            }
+ else if(day==6){
+        console.log("saturday")
+                }
+  else if(day==7){
+        console.log("sunday")
+                    }
+  else{
+    console.log("enter a valid inpot")
+  }
+
+
+
+//   switch(x){
+//   case 1 :------;
+//   case 2:-----;
+//   case 3 :------;
+//   case 4:-----;
+
+//   }
+
+
+
+let value="a";
+
+switch(value){
+case "a" : console.log("monday")
+           break
+case "b" : console.log("thusday")
+           break
+case "c" : console.log("wedneday");
+            break;
+case "d" : console.log("thusday");
+           break;
+case "e" : console.log("friday");
+           break;
+case "f" : console.log("saturday");
+           break;
+
+  default: console.log("plz enter the value from a-f")
+
+}
+
+
+
+
+
+
+
