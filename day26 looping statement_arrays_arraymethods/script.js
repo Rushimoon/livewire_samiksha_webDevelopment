1fd52a5cf0af69c0e01cdb726910932cd353f0ad
Rushimoon <rushikeshmoon1998@gmail.com>
2023-08-23 09:23:29 +0530

    update

---
+++ day26 looping statement_arrays_arraymethods/script.js
@@ -0,0 +1,65 @@
+// for while dowhile forin foreach .......
+
+
+for(let i=0;i<0;i++){
+
+
+    console.log(i)
+}
+
+// let j=0;
+// while(j<10){
+
+// console.log(j);
+
+// j++
+// }
+let k=0;
+
+do{
+    console.log(k);
+    k++
+}
+while(k<5);
+
+
+
+// Array 
+// int [] arr= new int [10];in java
+//        0    1  2  3  4
+let arr=["qwe",2,56,'2',5+6];
+console.log(arr)
+//arr.length --> it give the length of arr
+for(let i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
+
+//arr.push()-> it add element at the last of arr
+//arr.pop()-> it removes the element from last of arr
+//arr.unshift -> it adds the element to front of arr
+//arr.shift -> it removes the element from front of arr
+
+
+
+let fruits=["apple","bana nana","kiwi","oRANGE"]
+//           4003     4004        4005  4006
+
+fruits.push("Grapes");
+fruits.push("gqawa");
+console.log(fruits)
+// fruits.pop();
+// fruits.pop();
+// fruits.pop();
+
+fruits.shift()
+fruits.shift()
+fruits.shift()
+console.log(fruits)
+fruits.unshift("mausambi");
+fruits.unshift("cherri");
+fruits.unshift("lichi");
+fruits.unshift("mango");
+
+console.log(fruits)
+
