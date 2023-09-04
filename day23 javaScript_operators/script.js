5b101f7065f4fd7318ead5cf284c1746f8ac0918
Rushimoon <rushikeshmoon1998@gmail.com>
2023-09-04 09:14:52 +0530

    update

--- day23 javaScript_operators/script.js
+++ day23 javaScript_operators/script.js
@@ -1,41 +1,22 @@
-w=20;
-k=10.00005;
 
-console.log(w,k);
-
-// let var const;
-
-
-let dd=20;
-let m=50;
-var j="samiksha";
-let lende=" lende is samikshas surname"
-let d=lende;
-
- console.log(dd+" "+m+" "+j+" "+d)
- 
-
- const pi=3.14;
- let data=pi+1;
- console.log(data)
-//  pi=20;
-//  console.log(pi);
-let r=20;
-r=30;
-// Assign the value 5 to x
-let x = 5;
-// Assign the value 2 to y
-let y = 2;
-// Assign the value x + y to z:
-let z = x + y;
-
-
-console.log(z);
-let q=20;
-
-console.log(q++);
-console.log(q--);
-console.log(++q);
-console.log(--q);
 // -=-=-=-=-=-=-=-=-=-=-=-=
 // assingment operator
+
+
+// let x=20;
+// x+=20;
+// let y=10;
+// y*=8;
+// let k=80;
+// k%=2;
+// let x = 16 + volvo;
+let x = 16 + 4;
+console.log(x)
+
+function addtwoNum(a,b){
+
+    return a+b;
+}
+
+let ans=addtwoNum(5,6)
+console.log(ans)
---
+++ day27 Date time and Math/script.js
@@ -0,0 +1,48 @@
+let date=new Date();
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getTime())
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+let dateOfPoint=new Date('2001-12-21');
+// console.log(dateOfPoint.getFullYear());
+// console.log(dateOfPoint.getMonth()+1);
+// console.log(dateOfPoint.getDate());
+// console.log(dateOfPoint.getTime())
+// console.log(dateOfPoint.getHours())
+// console.log(dateOfPoint.getMinutes())
+// console.log(dateOfPoint.getSeconds())
+let setDateMeth=new Date();
+// console.log(setDateMeth.setFullYear('2013'));
+
+// console.log(setDateMeth.setMonth(11));
+
+// console.log(setDateMeth.setDate(21));
+// console.log(setDateMeth)
+// console.log(setDateMeth.setTime())
+// console.log(setDateMeth.setHours())
+// console.log(setDateMeth.setMinutes())
+// console.log(setDateMeth.setSeconds())
+
+
+//Math.round
+console.log(Math.round(4.4));
+//Math.floor()
+console.log(Math.floor(4.6))
+//math.ceil
+console.log(Math.ceil(4.6))
+
+
+
+let arr=[6,9,8,1,3,5]
+let x=-6+2;
+console.log(Math.sign(x))
+console.log(Math.pow(25,2))
+let num=625;
+console.log(Math.sqrt(num))
+console.log(Math.abs(x))
+console.log(Math.min(1,2,3,5,6))
+
+console.log(Math.max(1,2,3,5,6))
